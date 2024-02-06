4bc915a88b4fb1dd9030a350e94e8513bcf84602
danny <danny@AETHELRED>
2024-02-05 19:25:50 -0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program { 
+
+    static char cipher(char ch, int key)
+    {
+        if (!char.IsLetter(ch))
+        {
+            return ch;
+        }
+
+        char d = char.IsUpper(ch) ? 'A' : 'a';
+        return (char)((((ch + key) - d) % 26) + d);
+    }
+
+    static string Encipher(string input, int key)
+    {
+        string output = string.Empty;
+
+        foreach (char ch in input)
+            output += cipher(ch, key);
+
+        return output;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Type a string to encrypt:");
+        string UserString = Console.ReadLine();
+
+        Console.WriteLine("\n");
+
+
+        Console.WriteLine("Encrypted Data");
+
+        for (int i = 0; i < 26; i++)
+        {
+            string cipherText = Encipher(UserString, i);
+            Console.WriteLine("Shifting by {0} gives {1}", i, cipherText);
+
+        }
+    }
+}
